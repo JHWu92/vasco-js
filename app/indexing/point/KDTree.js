546180f4d478909f4658c870976224dbfce4fe11
JHWu92 <jhwu92@gmail.com>
2017-11-15 14:15:32 -0500

    update KDTree

--- app/indexing/point/KDTree.js
+++ app/indexing/point/KDTree.js
@@ -1,48 +1,310 @@
 /*global define*/
 
-define(['./constant', './KDNode'], function (cons, KDNode) {
-
+define(['./constant'], function (cons) {
     'use strict';
-    /* KDCompare in java applet*/
-    function leftOrRight(base, newNode) {
-        if (base.DISC === cons.xAttr) {
-            return (newNode.pt.x < base.pt.x) ? cons.left : cons.right;
-        } else {
-            return (newNode.pt.y < base.pt.y) ? cons.left : cons.right;
-        }
+
+    function KDTree(pt) {
+        this.pt = pt;
+        this.son = [null, null];
+        this.DISC = cons.xAttr; // split on which attribute, X or Y
     }
 
-    function addKDNode(root, newNode) {
-        var father = null,
-            curNode = null,
-            position = -1;
-        if (root === null) {
-            // first node for KD tree
-            root = newNode;
-            newNode.DISC = cons.xAttr;
-        } else {
-            // iterate KD tree to find a spot for newNode
-            curNode = root;
+    var pro = KDTree.prototype;
 
-            // curNode == null means empty empty spot for newNode; 
-            // newNode equals curNode means duplicate, don't add new node
-            while (curNode !== null && !newNode.pt.equals(curNode.pt)) {
-                father = curNode;
-                position = leftOrRight(curNode, newNode);
-                curNode = curNode.son[position];
-            }
-            if (curNode === null) {
-                father.son[position] = newNode;
-                newNode.DISC = father.nextDisc();
-            }
-        }
-        return root;
-    }
-/*
-    
-    */
-    
-    return {
-        addKDNode: addKDNode,
+    pro.nextDisc = function () {
+        return (this.DISC === cons.xAttr) ? cons.yAttr : cons.xAttr;
     };
+
+    pro.noLeftSon = function () {
+        return this.son[cons.left] === null;
+    };
+    pro.noRightSon = function () {
+        return this.son[cons.right] === null;
+    };
+    pro.empty = function () {
+        return this.noLeftSon() && this.noRightSon();
+    };
+
+    pro.directSonType = function (node) {
+        // TODO == comparison may not work here.
+        if (this.son[cons.left] === node) {
+            return cons.left;
+        } else if (this.son[cons.right] === node) {
+            return cons.right;
+        } else {
+            return null;
+        }
+    };
+    pro.directSonTypeByPt = function (pt) {
+        var leftpt = null,
+            rightpt = null;
+        if (!this.noLeftSon()) {
+            leftpt = this.son[cons.left].pt;
+        }
+        if (!this.noRightSon()) {
+            rightpt = this.son[cons.right].pt;
+        }
+
+        if (leftpt !== null && leftpt.equals(pt)) {
+            return cons.left;
+        } else if (rightpt !== null && rightpt.equals(pt)) {
+            return cons.right;
+        } else {
+            return null;
+        }
+    };
+
+    /* KDCompare in java applet*/
+    pro.leftOrRight = function (pt) {
+        if (this.DISC === cons.xAttr) {
+            return (pt.x < this.pt.x) ? cons.left : cons.right;
+        } else {
+            return (pt.y < this.pt.y) ? cons.left : cons.right;
+        }
+    };
+
+    /* code segment inside KDNode delete() */
+    pro.findNodeByPt = function (pt) {
+        var node = this,
+            sonType = null;
+        while (node !== null && !(node.pt.equals(pt))) {
+            sonType = node.leftOrRight(pt);
+            node = node.son[sonType];
+        }
+        return node;
+    };
+
+    /** 
+     * FindFather in vasco.KDTree. 
+     * FindFather(node, this, father)
+     * node: Target node, find the father for node.
+     * this: current node, if node===this, return father.
+     */
+    pro.findFather = function (node, father) {
+        //        console.log(node.toString(), this.toString());
+        if (node.pt.equals(this.pt)) {
+            return father;
+        }
+        var son = this.son[this.leftOrRight(node)];
+        return son.findFather(node, this);
+
+
+    };
+
+    /** recursively find node with minimum values in terms of disc*/
+    pro.findDMinimum = function (disc) {
+        function minNode(n1, n2, disc) {
+            if (n1 === null) {
+                return n2;
+            }
+            if (n2 === null) {
+                return n1;
+            }
+
+            if (disc === cons.xAttr) {
+                return (n1.pt.x < n2.pt.x) ? n1 : n2;
+            }
+            if (disc === cons.yAttr) {
+                return (n1.pt.y < n2.pt.y) ? n1 : n2;
+            }
+
+        }
+
+        var n1 = null,
+            n2 = null;
+
+        // if this is none, stop recursion
+        if (this === null) {
+            return null;
+        }
+
+        // if same split attribute
+        // because the attribute is the same
+        // descendant not larger than this itself are all LOSON
+        if (this.DISC === disc) {
+            // find a smaller node in the LOSON in terms of disc
+            if (!this.noLeftSon()) {
+                n1 = this.son[cons.left].findDMinimum(disc);
+            }
+            // compare the smallest in descendant with this itself
+            return minNode(this, n1, disc);
+        }
+
+        // if not the same split attribute
+        // minimum could be in either LOSON or HISON
+        if (!this.noLeftSon()) {
+            n1 = this.son[cons.left].findDMinimum(disc);
+        }
+        if (!this.noRightSon()) {
+            n2 = this.son[cons.right].findDMinimum(disc);
+        }
+        // find min among n1, n2 and this itself
+        return minNode(this, minNode(n1, n2, disc), disc);
+
+    };
+
+    pro.insert = function (pt) {
+        var newNode = null,
+            father = null,
+            curNode = this, // iterate KD tree to find a spot for pt
+            sontype = -1;
+
+
+        // curNode == null means empty empty spot for newNode; 
+        // newNode equals curNode means duplicate, don't add new node
+        while (curNode !== null && !pt.equals(curNode.pt)) {
+            father = curNode;
+            sontype = father.leftOrRight(pt);
+            curNode = father.son[sontype];
+        }
+
+        if (curNode === null) {
+            newNode = new KDTree(pt);
+            newNode.DISC = father.nextDisc();
+            father.son[sontype] = newNode;
+
+        }
+    };
+
+    pro.deleteByPt = function (pt) {
+        var rep, father, node;
+        node = this.findNodeByPt(pt);
+        rep = this.deleteHelperByNode(node);
+        father = this.findFather(node, null);
+        if (father === null) { // the node to be replaced is the root itself
+            this.pt = rep.pt;
+            this.DISC = rep.DISC;
+            this.son = rep.son;
+        } else {
+            father.son[father.directSonType(node)] = rep;
+        }
+    };
+
+    pro.deleteHelperByNode = function (node) {
+        var father = null, // original father of rep
+            rep = null, // replacement for deleted node
+            disc = null, // disc of deleted node
+            hiSon; // hiSon of node
+
+        // Stop recursion if node has no son
+        if (node.empty()) {
+            return null;
+        }
+
+        disc = node.DISC;
+
+        // if node has no HISON, move LOSON to HISON
+        if (node.son[cons.right] === null) {
+            node.son[cons.right] = node.son[cons.left];
+            node.son[cons.left] = null;
+        }
+
+        // find the node with minimun value in HISON 
+        // in terms of disc(node.DISC)
+        hiSon = node.son[cons.right];
+        rep = hiSon.findDMinimum(disc);
+        // find rep's current father in HISON
+        father = hiSon.findFather(rep, node);
+
+        // rep is supposed to replace node
+        // -> rep is effectively deleted from its original position
+        // -> recursively run the deleteByNode to trace down the left node
+        //    whose son is empty
+        // -> then fill up the empty slots from bottom to top.
+        father.son[father.directSonType(rep)] = this.deleteHelperByNode(rep);
+
+        // new root take over deleted node's son and DISC
+        rep.son[cons.left] = node.son[cons.left];
+        rep.son[cons.right] = node.son[cons.right];
+        // TODO: why not just disc?
+        rep.DISC = node.DISC;
+
+        return rep;
+    };
+
+    pro.getPartitions = function (minX, minY, maxX, maxY) {
+        var partition = [];
+        console.log(minX, minY, maxX, maxY);
+        if (this.empty()) {
+            return partition;
+        }
+        if (this.DISC === cons.xAttr) {
+            partition.push([this.pt.x, minY, this.pt.x, maxY]);
+            if (!this.noLeftSon()) {
+                Array.prototype.push.apply(partition,
+                    this.son[cons.left].getPartitions(minX, minY, this.pt.x, maxY)
+                );
+            }
+            if (!this.noRightSon()) {
+                Array.prototype.push.apply(partition,
+                    this.son[cons.right].getPartitions(this.pt.x, minY, maxX, maxY)
+                );
+            }
+
+        } else {
+            partition.push([minX, this.pt.y, maxX, this.pt.y]);
+            if (!this.noLeftSon()) {
+                Array.prototype.push.apply(partition, this.son[cons.left].getPartitions(minX, minY, maxX, this.pt.y));
+            }
+            if (!this.noRightSon()) {
+                Array.prototype.push.apply(partition, this.son[cons.right].getPartitions(minX, this.pt.y, maxX, maxY));
+            }
+
+        }
+        return partition;
+
+    };
+    pro.getBoundaryAsObj = function (minX, minY, maxX, maxY) {
+        var bnd = {};
+        if (this.empty()) {
+            return null;
+        }
+        if (this.DISC === cons.xAttr) {
+            bnd.me = [this.pt.x, minY, this.pt.x, maxY];
+            if (!this.noLeftSon()) {
+                bnd.left = this.son[cons.left].getBoundary(minX, minY, this.pt.x, maxY);
+            }
+            if (!this.noRightSon()) {
+                bnd.right = this.son[cons.right].getBoundary(this.pt.x, minY, maxX, maxY);
+            }
+
+        } else {
+            bnd.me = [minX, this.pt.y, maxX, this.pt.y];
+            if (!this.noLeftSon()) {
+                bnd.left = this.son[cons.left].getBoundary(minX, minY, maxX, this.pt.y);
+            }
+            if (!this.noRightSon()) {
+                bnd.right = this.son[cons.right].getBoundary(minX, this.pt.y, maxX, maxY);
+            }
+
+        }
+        return bnd;
+    };
+
+    // ------------------------
+    // print info about KDTree
+    // ------------------------
+
+    pro.toString = function () {
+        return this.pt.toString() + ' ' + this.DISC;
+    };
+
+    pro.treeJSON = function () {
+        var tree = {
+            node: this.toString()
+        };
+        if (!this.noLeftSon()) {
+            tree.left = this.son[cons.left].treeJSON();
+        }
+        if (!this.noRightSon()) {
+            tree.right = this.son[cons.right].treeJSON();
+        }
+        return tree;
+    };
+
+    pro.treeString = function () {
+        return JSON.stringify(this.treeJSON(), null, '  ');
+    };
+
+    return KDTree;
 });
